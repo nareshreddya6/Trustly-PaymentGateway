9a3f144c96f94c7e8addf516db8d930d32d3625b
nareshreddya6@gmail.com <asdfghjkl4520@A>
2025-02-08 10:31:11 +0530

    Added Models and Exception class

--- PaymentChecksHandler/src/main/java/PaymentChecksHandler/ChecksHandler/Constants/PaymentSaveOutcome.java
+++ PaymentChecksHandler/src/main/java/PaymentChecksHandler/ChecksHandler/Constants/PaymentSaveOutcome.java
@@ -1,4 +1,4 @@
-package main.java.PaymentChecksHandler.Constants;
+package main.java.PaymentChecksHandler.ChecksHandler.Constants;
 
 public class PaymentSaveOutcome {
     IS_SAVED, IS_DUPLICATE, IS_ERROR;
---
+++ PaymentChecksHandler/src/main/java/PaymentChecksHandler/ChecksHandler/models/ErrorResponse.java
@@ -0,0 +1,15 @@
+package main.java.PaymentChecksHandler.ChecksHandler.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String errorCode;
+    private String errorMessage;
+}
